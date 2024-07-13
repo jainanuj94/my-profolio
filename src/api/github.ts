8d541d898ca9e3dd1c2b01668c92ce27277e78c9
Anuj Jain <jainanuj94@Anujs-MacBook-Pro.local>
2024-07-14 00:38:37 +0530

    type cast for username

--- src/api/github.ts
+++ src/api/github.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const GITHUB_API_URL = 'https://api.github.com';
 
-export const fetchRepositories = async (username) => {
+export const fetchRepositories = async (username: string) => {
     try {
         const response = await axios.get(`${GITHUB_API_URL}/users/${username}/repos`);
         return response.data;
